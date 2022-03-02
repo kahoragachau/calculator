0174dbf52114accd83a76ee1b0cdf0d429170cc0
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-02 15:09:06 +0300

    add the divide function to the script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,9 @@ var subtract = function(number1, number2){
 var multiply = function(number1,number2){
     return number1 * number2;
 }
+var divide = function(number1, number2){
+    return number1 / number2
+}
 var number1 = parseInt(prompt("Enter a number: "));
 var number2 = parseInt(prompt("Enter a second Number: "));
 var result = add(number1,number2);
