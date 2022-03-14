05a2543af53bf29af39eddae79b9ce7e37002938
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-14 14:32:07 +0300

    - Add Subtraction Form

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,22 @@ var divide = function(number1, number2){
 
 // User Interface Logic
 $(document).ready(function(){
+    // Form Add
+
     $('form#add').submit(function(event){
         var number1 = parseInt($("#add1").val());
         var number2 = parseInt($("#add2").val());
         var result = add(number1,number2);
-        $('#output').text(result);
+        $('#add-output').text(result);
         event.preventDefault();
     });
+
+    // Form Subtract
+    $('form#subtract').submit(function(event){
+        var number1 = parseInt($("#sub1").val());
+        var number2 = parseInt($("#sub2").val());
+        var result = subtract(number1,number2);
+        $('#sub-output').text(result);
+        event.preventDefault();
+    })
 });
